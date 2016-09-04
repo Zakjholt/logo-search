e576058b61ac32262ffc4e3fb8f31bcb0277ef15
zak <zak.j.holt@gmail.com>
2016-09-04 14:53:09 -0400

    met requirements of project

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
-  $("form").submit(function(e) {
-    e.preventDefault();
-  });
+    $("form").submit(function(e) {
+        e.preventDefault();
+        if ($("#programType").val() != "Program Type") {
+          programType = "." + $("#programType").val();
+        } else {
+          programType = "";
+        }
+
+        if ($("#educationLevel").val() != "Education Level") {
+          educationLevel = "." + $("#educationLevel").val();
+        } else {
+          educationLevel = "";
+        }
+        $(".greyScale").show();
+        $(".color").hide();
+        $(programType + educationLevel  + ".greyScale").hide();
+        $(programType +  educationLevel + ".color").fadeIn();
+    });
 });
